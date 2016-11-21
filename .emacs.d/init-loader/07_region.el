993b7f710ee8b1da5ff8bb1708a691bb46a71adf
zmm76933 <zmm76933@gmail.com>
2016-11-21 19:45:48 +0900

    Add region isearch

--- .emacs.d/init-loader/07_region.el
+++ .emacs.d/init-loader/07_region.el
@@ -14,4 +14,17 @@
 (global-set-key (kbd "C-.") 'er/expand-region)
 (global-set-key (kbd "C-,") 'er/contract-region)
 
+;; isearch in region
+(defadvice isearch-mode (around isearch-mode-default-string (forward &optional regexp op-fun recursive-edit word-p) activate)
+  (if (and transient-mark-mode mark-active (not (eq (mark) (point))))
+      (progn
+        (isearch-update-ring (buffer-substring-no-properties (mark) (point)))
+        (deactivate-mark)
+        ad-do-it
+        (if (not forward)
+            (isearch-repeat-backward)
+          (goto-char (mark))
+          (isearch-repeat-forward)))
+    ad-do-it))
+
 ;;; region.el ends here
