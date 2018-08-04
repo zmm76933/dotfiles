3555d7236a0c9e8120e5f2137579dd7c3794b144
zmm76933 <zmm76933@gmail.com>
2018-08-05 07:52:20 +0900

    Add ediff keymap in emacs

--- .emacs.d/init-loader/18_diff.el
+++ .emacs.d/init-loader/18_diff.el
@@ -16,4 +16,15 @@
  '(ediff-split-window-function 'split-window-horizontally)
  '(ediff-diff-options "-twB"))
 
+(defun keymap-ediff-hook ()
+  (ediff-setup-keymap)
+  (define-key ediff-mode-map "j" 'ediff-next-difference)
+  (define-key ediff-mode-map "k" 'ediff-previous-difference)
+  (define-key ediff-mode-map "z" 'ediff-scroll-vertically)
+  (define-key ediff-mode-map "C-z" 'ediff-scroll-vertically))
+
+(add-hook 'ediff-mode-hook 'keymap-ediff-hook)
+
+
+
 ;;; diff.el ends here
