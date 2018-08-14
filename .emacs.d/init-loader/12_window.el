b92b66477b3518d3585fa444a61a920f9b514830
zmm76933 <zmm76933@gmail.com>
2018-08-14 13:53:08 +0900

    Add helm popup window in Emacs

--- .emacs.d/init-loader/12_window.el
+++ .emacs.d/init-loader/12_window.el
@@ -9,37 +9,42 @@
 ;; Window configuration
 
 ;; popwin
-(require 'popwin)
+(when (require 'popwin)
 
-(defvar popwin:special-display-config-backup popwin:special-display-config)
-(custom-set-variables
- '(display-buffer-function 'popwin:display-buffer))
+  (defvar popwin:special-display-config-backup popwin:special-display-config)
+  (custom-set-variables
+   '(display-buffer-function 'popwin:display-buffer))
 
-;; remove from default config
-(dolist (stuff '("*vc-diff*" "*vc-change-log*"))
-  (delete stuff popwin:special-display-config))
+  ;; remove from default config
+  (dolist (stuff '("*vc-diff*" "*vc-change-log*"))
+    (delete stuff popwin:special-display-config))
 
-;; basic
-(push '("*Help*" :stick t :noselect t) popwin:special-display-config)
-(push '("*sdic*") popwin:special-display-config)
+  ;; basic
+  (push '("*Help*" :stick t :noselect t) popwin:special-display-config)
+  (push '("*complitation*" :noselect t) popwin:special-display-config)
+  (push '("*sdic*") popwin:special-display-config)
 
-;; python
-(push '("*Python*"   :stick t) popwin:special-display-config)
-(push '("*Python Help*" :stick t :height 20) popwin:special-display-config)
-(push '("*jedi:doc*" :stick t :noselect t) popwin:special-display-config)
 
-;; Go
-(push '("^\*go-direx:" :position left :width 0.3 :dedicated t :stick t :regexp t)
-      popwin:special-display-config)
+  ;; python
+  (push '("*Python*"   :stick t) popwin:special-display-config)
+  (push '("*Python Help*" :stick t :height 20) popwin:special-display-config)
+  (push '("*jedi:doc*" :stick t :noselect t) popwin:special-display-config)
 
-;; flycheck
-(push '(flycheck-error-list-mode :stick t) popwin:special-display-config)
+  ;; Go
+  (push '("^\*go-direx:" :position left :width 0.3 :dedicated t :stick t :regexp t)
+        popwin:special-display-config)
 
-;; quickrun
-(push '("*quickrun*" :stick t) popwin:special-display-config)
+  ;; flycheck
+  (push '(flycheck-error-list-mode :stick t) popwin:special-display-config)
 
-;; direx
-(push '(direx:direx-mode :position left :width 30 :dedicated t)
-      popwin:special-display-config)
+  ;; quickrun
+  (push '("*quickrun*" :stick t) popwin:special-display-config)
+
+  ;; direx
+  (push '(direx:direx-mode :position left :width 30 :dedicated t)
+        popwin:special-display-config)
+
+  ;; helm
+  (push '("helm" :regexp t :height 0.4) popwin:special-display-config))
 
 ;;; window.el ends here
