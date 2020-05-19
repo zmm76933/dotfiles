461a8cc11cd15f4b94650702214da6eb2584a1f0
zmm76933 <zmm76933@gmail.com>
2020-05-19 14:11:59 +0900

    Clean up

--- .emacs.d/init-el-get.el
+++ .emacs.d/init-el-get.el
@@ -150,11 +150,6 @@
 (el-get-bundle syohex/emacs-git-gutter2 :name git-gutter2)
 (el-get-bundle syohex/emacs-git-messenger2 :name git-messenger2)
 
-;; Documentation
-(if (eq system-type 'darwin)
-    (el-get-bundle dash-at-point)
-  (el-get-bundle zeal-at-point))
-
 ;; key
 (el-get-bundle which-key)
 
@@ -162,8 +157,6 @@
 (el-get-bundle emacs-helm/helm-descbinds)
 (el-get-bundle syohex/emacs-helm-gtags :name helm-gtags)
 (el-get-bundle syohex/emacs-helm-ag2 :name helm-ag2)
-;; (el-get-bundle syohex/emacs-helm-pydoc :name helm-pydoc)
-;; (el-get-bundle syohex/emacs-helm-perldoc :name helm-perldoc)
 (el-get-bundle syohex/emacs-helm-godoc :name helm-godoc)
 (el-get-bundle ShingoFukuyama/helm-swoop :name helm-swoop)
 
