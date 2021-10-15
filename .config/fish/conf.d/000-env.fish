3bc0d56fa9710f043ff2a0ae0f934af2bf9b775d
zmm76933 <zmm76933@gmail.com>
2021-10-15 19:53:38 +0900

    fix(fish): add fish repository in linux

--- .config/fish/conf.d/000-env.fish
+++
@@ -1,2 +0,0 @@
-set -x PATH $HOME/.pyenv/bin $PATH
-set -x PATH $HOME/.rbenv/bin $PATH
--- etc/init/linux/packages.sh
+++ etc/init/linux/packages.sh
@@ -9,7 +9,7 @@ set -eu
 # For more information, see etc/README.md
 . "$DOTPATH"/etc/lib/vital.sh
 
-PACKAGES="git fish tmux
+PACKAGES="git tmux
     build-essential diffutils autoconf automake silversearcher-ag
     g++ libxml2-dev libssl-dev libsqlite3-dev libevent-dev
     libsensors4-dev libavahi-common-dev libavahi-client-dev libncurses-dev libnss-mdns
@@ -18,7 +18,7 @@ PACKAGES="git fish tmux
     sysstat curl w3m wget nmap wakeonlan testdisk fortunes-ubuntu-server
     openssh-server avahi-daemon isc-dhcp-server samba cifs-utils"
 
-REPOSITORY="neovim/focal"
+REPOSITORY="fish neovim/focal"
 
 if has "yum"; then
     log_echo "Install packages with Yellowdog Updater Modified"
@@ -27,6 +27,7 @@ elif has "apt"; then
     log_echo "Install packages with Advanced Packaging Tool"
     sudo apt -y install $PACKAGES
     if has "add-apt-repository"; then
+        sudo apt-add-repository -y ppa:fish-shell/release-3
         sudo add-apt-repository -y ppa:neovim-ppa/unstable
         sudo apt update
         sudo apt -y install $REPOSITORY
