684437e742d6596f1fb701b41b01c451c882a6cc
zmm76933 <zmm76933@gmail.com>
2021-11-07 19:17:08 +0900

    fix(fish): fix brew doctor warning "config"

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -1,14 +1,6 @@
 # Homebrew
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-
-function brew
-    set -xl PATH $PATH # Protect global PATH by local PATH
-    if type -q pyenv; and contains (pyenv root)/shims $PATH
-        set -e PATH[(contains -i (pyenv root)/shims $PATH)]
-    end
-
-    command brew $argv
-end
+alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
 
 if type -q yum
   alias update "sudo yum -y update && yum clean all"
--- .config/fish/config-macos.fish
+++ .config/fish/config-macos.fish
@@ -2,13 +2,5 @@
 eval (/usr/local/bin/brew shellenv)
 set -gx HOMEBREW_CASK_OPTS "--appdir=/Applications"
 
-function brew
-    set -xl PATH $PATH # Protect global PATH by local PATH
-    if type -q pyenv; and contains (pyenv root)/shims $PATH
-        set -e PATH[(contains -i (pyenv root)/shims $PATH)]
-    end
-
-    command brew $argv
-end
-
+alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
 alias update "brew update && brew upgrade && brew cleanup"
