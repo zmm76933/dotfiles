b5776150c6b57e4c485c2e1a9866c46851a589b6
zmm76933 <zmm76933@gmail.com>
2022-08-16 13:47:32 +0900

    feat(nvim): add wsl clipboard

--- .config/nvim/lua/macos.lua
+++ .config/nvim/lua/macos.lua
@@ -1,3 +1,4 @@
 vim.opt.clipboard:append { 'unnamedplus' }
 
+-- Improve Vim/Neovim experience with input methods
 vim.api.nvim_set_var('im_select_default', 'jp.sourceforge.inputmethod.aquaskk.Ascii')
--- .config/nvim/lua/wsl.lua
+++ .config/nvim/lua/wsl.lua
@@ -1,3 +1,19 @@
+vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
+
 -- Improve Vim/Neovim experience with input methods
 vim.api.nvim_set_var('im_select_command', '/mnt/c/ProgramData/zenhan/zenhan.exe')
 vim.api.nvim_set_var('im_select_default', 0)
+
+-- Use windows clipboard to copy and to paste
+vim.api.nvim_set_var('clipboard', {
+  name = 'win32yank-wsl',
+  copy = {
+    ['+'] = '/mnt/c/ProgramData/win32yank/win32yank.exe -i --crlf',
+    ['*'] = '/mnt/c/ProgramData/win32yank/win32yank.exe -i --crlf'
+  },
+  paste = {
+    ['+'] = '/mnt/c/ProgramData/win32yank/win32yank.exe -o --lf',
+    ['*'] = '/mnt/c/ProgramData/win32yank/win32yank.exe -o --lf'
+  },
+  cache_enabled = 0,
+})
