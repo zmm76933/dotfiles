7cf2fb3528a33ab47e10a15193fcc8748cd953b0
zmm76933 <zmm76933@gmail.com>
2022-08-16 11:41:02 +0900

    fix(nvim): fix command name

--- .config/nvim/lua/wsl.lua
+++ .config/nvim/lua/wsl.lua
@@ -1,3 +1,3 @@
 -- Improve Vim/Neovim experience with input methods
-vim.api.nvim_set_var('im_select-Command', '/mnt/c/ProgramData/zenhan/zenhan.exe')
+vim.api.nvim_set_var('im_select_command', '/mnt/c/ProgramData/zenhan/zenhan.exe')
 vim.api.nvim_set_var('im_select_default', 0)
