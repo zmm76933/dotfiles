0797e3b12136e74bbd9da126e512f746db20725a
Masato Katayama <zmm76933@gmail.com>
2023-05-03 11:45:03 +0900

    fix(nvim): add typescript language and enable context_commentstring

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -16,6 +16,7 @@ ts.setup {
     "markdown",
     "markdown_inline",
     "tsx",
+    "typescript",
     "toml",
     "fish",
     "php",
@@ -30,6 +31,10 @@ ts.setup {
   autotag = {
     enable = true,
   },
+  context_commentstring = {
+    enable         = true,
+    enable_autocmd = false,
+  }
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
