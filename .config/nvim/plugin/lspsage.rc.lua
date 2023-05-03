c49faed164ce32eba3f10b2f7dfa4a40d4af4bc1
Masato Katayama <zmm76933@gmail.com>
2023-05-03 11:41:35 +0900

    fix(nvim): show_diagnostic not working
    use `show_line_diagnostics`

--- .config/nvim/plugin/lspsage.rc.lua
+++ .config/nvim/plugin/lspsage.rc.lua
@@ -15,7 +15,7 @@ local diagnostic = require("lspsaga.diagnostic")
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', ']g', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', '[g', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
-vim.keymap.set('n', '<C-k>l', '<Cmd>Lspsaga show_diagnostics<CR>', opts)
+vim.keymap.set('n', '<C-k>l', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', '<C-k>d', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 --vim.keymap.set('i', '<C-k>h', '<Cmd>Lspsaga signature_help<CR>', opts)
