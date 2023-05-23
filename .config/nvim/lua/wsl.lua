4168010319242dbc35c875a3ad7d15babb66d241
Masato Katayama <zmm76933@gmail.com>
2023-05-23 12:04:09 +0900

    feat(nvim): apply new im-select plugin's functions

--- .config/nvim/lua/wsl.lua
+++ .config/nvim/lua/wsl.lua
@@ -3,20 +3,30 @@ vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
 -- Improve Vim/Neovim experience with input methods
 require('im_select').setup {
 
-  -- IM will be set to `default_im_select` in `normal` mode(`EnterVim` or `InsertLeave`)
+  -- IM will be set to `default_im_select` in `normal` mode
   -- For Windows/WSL, default: "1033", aka: English US Keyboard
   -- For macOS, default: "com.apple.keylayout.ABC", aka: US
-  -- You can use `im-select` in cli to get the IM's name you preferred
+  -- For Linux, default: "keyboard-us"
+  -- You can use `im-select` or `fcitx5-remote -n` to get the IM's name you preferred
   default_im_select = '0',
-
-  -- Set to 1 if you don't want restore IM status when `InsertEnter`
-  disable_auto_restore = 0,
 
   -- Can be binary's name or binary's full path,
   -- e.g. 'im-select' or '/usr/local/bin/im-select'
   -- For Windows/WSL, default: "im-select.exe"
   -- For macOS, default: "im-select"
-  default_command = '/mnt/c/ProgramData/zenhan/zenhan.exe'
+  -- For Linux, default: "fcitx5-remote"
+  default_command = '/mnt/c/ProgramData/zenhan/zenhan.exe',
+
+  -- Restore the default input method state when the following events are triggered
+  set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
+
+  -- Restore the previous used input method state when the following events are triggered
+  -- if you don't want to restore previous used im in Insert mode,
+  -- e.g. deprecated `disable_auto_restore = 1`, just let it empty `set_previous_events = {}`
+  set_previous_events = {},
+
+  -- Show notification about how to install executable binary when binary is missing
+  keep_quiet_on_no_binary = false
 }
 
 -- Use windows clipboard to copy and to paste
