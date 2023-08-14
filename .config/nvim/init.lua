cc28dd6ab66fc3c4e7831a76a268bd4219601c4e
Masato Katayama <zmm76933@gmail.com>
2023-08-15 08:09:15 +0900

    fix(nvim): add linux config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,11 +7,15 @@ local has = function(x)
   return vim.fn.has(x) == 1
 end
 local is_mac = has "macunix"
+local is_linux = has "unix"
 local is_win = has "win32"
 local is_wsl = has "wsl"
 
 if is_mac then
   require('macos')
+end
+if is_linux == 1 then
+  require('linux')
 end
 if is_win then
   require('windows')
