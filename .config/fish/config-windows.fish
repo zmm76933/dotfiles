cad83570ad4d110c954fadee846bf2086a00bf4e
Masato Katayama <zmm76933@gmail.com>
2024-01-02 14:29:59 +0900

    feat(fish): add vagrant settings in windows powershell

--- .config/fish/config-windows.fish
+++ .config/fish/config-windows.fish
@@ -1,2 +1,8 @@
-alias code '"/mnt/c/Users/zmm76933/AppData/Local/Programs/Microsoft VS Code/bin/code"'
+# vagrant
+set -gx VAGRANT_WSL_ENABLE_WINDOWS_ACCESS 1
+set -gx PATH "/mnt/c/Program Files/Oracle/VirtualBox" $PATH
+set -gx PATH /mnt/c/Windows/System32 $PATH
+set -gx PATH "/mnt/c/Windows/System32/WindowsPowerShell/v1.0" $PATH
+
+alias code "/mnt/c/Users/zmm76933/AppData/Local/Programs/Microsoft VS Code/bin/code"
 alias clip "/mnt/c/Windows/System32/clip.exe"
