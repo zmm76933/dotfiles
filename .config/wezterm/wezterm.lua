0290a38b17419e46479c1501c9b73fc2beaa3937
Masato Katayama <zmm76933@gmail.com>
2024-05-26 09:12:26 +0900

    feat(wezterm): add wezterm settings

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- colors
+config.color_scheme = "nord"
+-- font
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 12.0
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Tokyo Night'
+
+config.hide_tab_bar_if_only_one_tab = true
+
+-- and finally, return the configuration to wezterm
+return config
