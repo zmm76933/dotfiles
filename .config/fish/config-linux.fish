db4d37592848b1ae4084208fbc904b15aab1877f
Masato Katayama <zmm76933@gmail.com>
2024-07-02 10:37:20 +0900

    fix(brew): change update alias in linux

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -6,8 +6,4 @@ eval (cat /etc/environment | sed -e 's/PATH/set -x PATH/' -e 's/[:=]/ /g' -e 's/
 set -e HOMEBREW_SHELLENV_PREFIX
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-if type -q dnf
-    alias update "sudo dnf -y update && dnf clean all"
-else if type -q apt
-    alias update "brew update && brew upgrade && brew cleanup && sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade && sudo apt -y autoremove && sudo apt -y autoclean"
-end
+alias update "brew update && brew upgrade && brew cleanup"
