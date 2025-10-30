ce7ef3cdbf19581eb8f9e278897e406746a11c27
Masato Katayama <zmm76933@gmail.com>
2025-10-30 12:04:21 +0900

    fix(nvim): fix lsp keymap

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -153,19 +153,16 @@ return {
   },
 	{
     "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      vim.list_extend(keys, {
-        {
-          "gd",
-          function()
-            -- DO NOT RESUSE WINDOW
-            require("telescope.builtin").lsp_definitions({ reuse_win = false })
-          end,
+    opts = {
+      servers = {
+        ['*'] = {
+          keys = {
+            { "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", has = "definition"},
+          },
           desc = "Goto Definition",
           has = "definition",
         },
-      })
-    end,
+      },
+    },
   },
 }
